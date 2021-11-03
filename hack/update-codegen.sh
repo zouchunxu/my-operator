133f1e4564e68ff1f92b2f0d18a5941e235c56f7
Chunxu Zou <471500439@qq.com>
2021-11-03 22:47:00 +0800

    sb

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 # Copyright 2017 The Kubernetes Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -16,16 +18,18 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-CODEGEN_PKG="/Users/zouchunxu/web/docker/wwwroot/go/code-generator"
+SCRIPT_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
+CODEGEN_PKG=${CODEGEN_PKG:-$(cd "${SCRIPT_ROOT}"; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../code-generator)}
 
 # generate the code with:
 # --output-base    because this script should also be able to run inside the vendor dir of
 #                  k8s.io/kubernetes. The output-base is needed for the generators to output into the vendor dir
 #                  instead of the $GOPATH directly. For normal projects this can be dropped.
 bash "${CODEGEN_PKG}"/generate-groups.sh "deepcopy,client,informer,lister" \
-  k8s.io/sample-controller/pkg/generated k8s.io/sample-controller/pkg/apis \
-  samplecontroller:v1alpha1 \
-  --output-base "/Users/zouchunxu/web/docker/wwwroot/go/my-operator"
+  go/my-operator/pkg/generated go/my-operator/pkg/apis \
+  samplecrd:v1 \
+  --output-base "$(dirname "${BASH_SOURCE[0]}")/../../.." \
+  --go-header-file "${SCRIPT_ROOT}"/hack/boilerplate.go.txt
 
 # To use your own boilerplate text append:
 #   --go-header-file "${SCRIPT_ROOT}"/hack/custom-boilerplate.go.txt
