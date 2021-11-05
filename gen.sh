b92cb7358e000d4691f8ff5657ce1ded163971bc
Chunxu Zou <471500439@qq.com>
2021-11-05 22:36:01 +0800

    is ok

--- gen.sh
+++
@@ -1,9 +0,0 @@
-ROOT_PACKAGE="/Users/zouchunxu/web/docker/wwwroot/go/my-operator"
-
-CUSTOM_RESOURCE_NAME="samplecrd"
-
-
-CUSTOM_RESOURCE_VERSION="v1"
-
-
-./generate-groups.sh all "$ROOT_PACKAGE/pkg/client" "$ROOT_PACKAGE/pkg/apis" "$CUSTOM_RESOURCE_NAME:$CUSTOM_RESOURCE_VERSION"
